3198b8ae8472afcb32531dc47810f79aa12d1344
Caitlyn Peace <peaccm22@wclive.westminster.edu>
2018-04-04 12:15:55 -0400

    Coded the Register Activity to account for inputted information & save it, DOES NOT WORK CORRECTLY.

--- app/src/main/java/com/westminster/caitlynpeace/chirp/RegisterActivity.java
+++ app/src/main/java/com/westminster/caitlynpeace/chirp/RegisterActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class RegisterActivity extends AppCompatActivity
 {
@@ -25,16 +30,41 @@ public class RegisterActivity extends AppCompatActivity
         TextView tv = findViewById(R.id.text);
         tv.setText(label);
 
-        Button reg = findViewById(R.id.RegisterButton);
-        reg.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                Intent r = new Intent(RegisterActivity.this, ViewRecentChirpsActivity.class);
-                r.putExtra(RegisterActivity.LABEL_KEY,"View Recent Chirps");
-                startActivity(r);
-            }
-        });
+        // Get newly registered user's email
+        EditText inputEmail = (EditText) findViewById(R.id.userEmail);
+        Editable userEmail = inputEmail.getText();
+
+        //Get newly registered user's handle
+        EditText inputHandle = (EditText) findViewById(R.id.userHandle);
+        Editable userHandle = inputEmail.getText();
+
+        // Get newly registered users' password
+        EditText inputPass = (EditText) findViewById(R.id.userPassword);
+        Editable userPassword = inputPass.getText();
+
+        //Get confirmed password & check if it is identical
+        EditText confirmPass = (EditText) findViewById(R.id.confirmUserPassword);
+        Editable confPass = inputPass.getText();
+
+        if(!inputPass.equals(confirmPass))
+        {
+            Toast.makeText(RegisterActivity.this,
+                    "Passwords do NOT match!", Toast.LENGTH_LONG).show();
+        }
+        else if(inputPass.equals(confirmPass))
+        {
+            Button reg = findViewById(R.id.RegisterButton);
+            reg.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view)
+                {
+                    Intent r = new Intent(RegisterActivity.this, ViewRecentChirpsActivity.class);
+                    r.putExtra(RegisterActivity.LABEL_KEY,"View Recent Chirps");
+                    startActivity(r);
+                }
+            });
+        }
+
         
     }
 }
