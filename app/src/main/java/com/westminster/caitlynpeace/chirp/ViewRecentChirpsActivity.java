cfaea761a0974e3cfc7bc27376a10062732a91ea
Alexander Michels <alexandercm4297@gmail.com>
2018-04-05 16:50:13 -0400

    Got RecyclerView written

--- app/src/main/java/com/westminster/caitlynpeace/chirp/ViewRecentChirpsActivity.java
+++ app/src/main/java/com/westminster/caitlynpeace/chirp/ViewRecentChirpsActivity.java
@@ -5,10 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by caitlynpeace on 3/23/18.
@@ -20,12 +27,15 @@ public class ViewRecentChirpsActivity extends AppCompatActivity {
     public static final String USER_EMAIL = "USER_EMAIL";
     public static final String USER_PASSWORD = "USER_PASSWORD";
 
+    private String userEmail;
+    private String userPassword;
+
     //just a change of scope local variables->instance variables
     private RecyclerView timeline;
     private LinearLayoutManager timelineManager;
-    private
     private Button logoutButton;
     private Button editWatchingButton;
+
 
     private int x;
 
@@ -35,6 +45,8 @@ public class ViewRecentChirpsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_viewrecentchirps);
 
         timeline = findViewById(R.id.timeline_recyclerview);
+        timelineManager = new LinearLayoutManager(this);
+        timeline.setLayoutManager(timelineManager);
 
 
         logoutButton = findViewById(R.id.logout);
@@ -58,6 +70,53 @@ public class ViewRecentChirpsActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public static ArrayList<Chirp> getChirps()
+    {
+        //figure this out.
+        return null;
+    }
+
+    public static class ChirpViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView creatorTextView;
+        private TextView messageTextView;
+        private int index;
+        public ChirpViewHolder(LayoutInflater inflater, ViewGroup parent)
+        {
+            super(inflater.inflate(R.layout.chirp, parent, false));
+            creatorTextView = itemView.findViewById(R.id.chirp_creator);
+            messageTextView = itemView.findViewById(R.id.chirp_message);
+            //set on click listener later?
+        }
+
+        public void bind(int index)
+        {
+            Chirp c = getChirps().get(index);
+            creatorTextView.setText(c.getCreator());
+            messageTextView.setText(c.getMessage());
+            this.index = index;
+        }
+    }
+
+    public class ChirpAdapter extends RecyclerView.Adapter<ChirpViewHolder>
+    {
+        @Override
+        public ChirpViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(ViewRecentChirpsActivity.this);
+            return new ChirpViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ChirpViewHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return getChirps().size();
+        }
     }
 }
 
