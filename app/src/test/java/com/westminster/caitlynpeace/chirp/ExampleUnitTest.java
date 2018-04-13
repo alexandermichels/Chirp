17f5a7a38acd7e61cc3f6bd5d1ea1b84f1d7bbbc
Alexander Michels <alexandercm4297@gmail.com>
2018-04-12 23:47:10 -0400

    Added a StringUtil to support encryption and hashing for passwords in logging in

--- app/src/test/java/com/westminster/caitlynpeace/chirp/ExampleUnitTest.java
+++ app/src/test/java/com/westminster/caitlynpeace/chirp/ExampleUnitTest.java
@@ -4,14 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+public class ExampleUnitTest
+{
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void isValidUser_test1()
+    {
         assertEquals(4, 2 + 2);
     }
 }
